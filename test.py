c9d7e5875aab92e686381da0e291b0e8035e091f
June Simmons <61482672+JuneTheMistborn@users.noreply.github.com>
2021-03-21 18:42:21 -0500

    Delete test.py
    removing testing file

--- test.py
+++
@@ -1,7 +0,0 @@
-string = "19.9679534"
-
-verify = lambda s: False if s.islower() or s.isupper()\
-    else f"{(round(float(s), 4)):.4f}" + "%" if 250 >= float(s) >= 0 else False
-
-print(verify(string))
-
