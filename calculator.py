df991104f922c975ac81c351529bef85201fb10e
Sirius-Aegislink <61482672+JuneTheMistborn@users.noreply.github.com>
2021-03-22 18:17:40 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,54 +1,52 @@
-# Satisfactory production line calculator
-# Author: June Simmons
-# 3/21/2021
-# Allows calculation of how many items you need to input at a given overclock percentage,
-# or the overclock percentage to create the given number of items,
-# given how many items per minute it (needs/produces) at given overclock percentage
+"""
+Satisfactory production line calculator
+Author: June Simmons
+3/21/2021
+Allows calculation of how many items you need to input at a given overclock percentage,
+or the overclock percentage to create the given number of items,
+given how many items per minute it (needs/produces) at given overclock percentage
+"""
 
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image
+import tkinter.font as tkFont
 
 
 # Initializing window
 class Calculator:
     def __init__(self):
-        self.root = Tk()
+        self.root = tk.Tk()
+        self.root.minsize(850, 778)
         self.root.title("Satisfactory Production Calculator")
         self.root.configure(bg="#5F668C")
-        self.root.attributes("-fullscreen", True)
 
         # Placing the background image in the center of the screen
-        self.canvas = Canvas(self.root, width=850, height=778, highlightthickness=0)
+        self.canvas = tk.Canvas(self.root, width=850, height=778, highlightthickness=0)
         self.img = ImageTk.PhotoImage(Image.open("Recipe.png"))
-        self.canvas.create_image(0, 0, anchor=NW, image=self.img)
-        self.canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+        self.canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         # Entry based overclocking
-        self.overclockIn = Entry(fg="#E59345", width=9)
+        self.font = tk.font.Font(family="Myriad Pro", size=20)
+        self.overclockIn = tk.Entry(bd=0, fg="#E59345", width=9, font=self.font)
         self.overclockIn.insert(0, "100.0000%")
-        self.overclockIn.place(x=710, y=734)
+        self.overclockIn.place(x=175, y=593)
 
         self.root.bind("<Return>", (lambda event: self.ValidateOverclock(self.overclockIn.get())))
 
-        self.overclockValid = Label(bd=0, fg="#278E3A", text="The overclock percentage is valid!")
-        self.overclockValid.place(x=770, y=734)
+        self.overclockValid = tk.Label(bd=0, fg="#278E3A", text="The overclock percentage is valid!")
+        self.overclockValid.place(x=435, y=593)
 
         # Slider based overclocking
-        self.overclockSlider = Scale(command=self.ValidateOverclock, troughcolor="#FA9549", orient="horizontal",
-                                     from_=0.0, to=250.0, length=500, width=45, sliderlength=10, bd=5,
-                                     highlightthickness=0, tickinterval=50.0)
+        self.overclockSlider = tk.Scale(command=self.ValidateOverclock, troughcolor="#FA9549", orient="horizontal",
+                                        from_=0.0, to=250.0, length=500, width=45, sliderlength=10, bd=5,
+                                        highlightthickness=0, tickinterval=50.0, showvalue=0)
         self.overclockSlider.set(100)
-        self.overclockSlider.place(x=705, y=766)
-
-        self.exitBtn = Button(self.root, text="Quit", command=self.Close, bg="#E41B1E", activebackground="#e41b1e")
-        self.exitBtn.place(x=0, y=0)
+        self.overclockSlider.place(x=170, y=639)
 
         self.root.mainloop()
 
-    # Defining function to be called on quit button press and
     # function to validate the input number for overclock percentage
-    def Close(self):
-        self.root.destroy()
 
     def ValidateOverclock(self, overclock_var):
         try:
@@ -56,17 +54,17 @@ class Calculator:
                 overclock = f"{(float(overclock_var.strip('%'))):.4f}"
                 self.overclockValid.configure(text="The overclock percentage is valid!", fg="#278E3A")
                 if float(overclock) == round(float(self.overclockIn.get().strip("%"))):
-                    pass
+                    self.overclockSlider.set(float(overclock))
                 elif float(overclock) != round(float(self.overclockIn.get().strip("%"))):
                     self.overclockIn.delete(0, "end")
                     self.overclockIn.insert(0, overclock + "%")
-                    self.overclockSlider.set(float(overclock))
+
             else:
                 self.overclockValid.configure(text="The overclock percentage is not valid!\nPlease enter a float "
-                                                   "between 0 and 250 with 4 or less decimal places.", fg="#B42E2C")
+                                                   "between 0 and 250\nwith 4 or less decimal places.", fg="#B42E2C")
         except ValueError:
             self.overclockValid.configure(text="The overclock percentage is not valid!\nPlease enter a float between 0 "
-                                               "and 250 with 4 or less decimal places.", fg="#B42E2C")
+                                               "and 250\nwith 4 or less decimal places.", fg="#B42E2C")
 
 
 app = Calculator()
