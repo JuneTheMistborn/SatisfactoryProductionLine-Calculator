102cfa7e97e153a34d127c8a30608e0500447372
Sirius-Aegislink <61482672+JuneTheMistborn@users.noreply.github.com>
2021-03-26 12:59:09 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -12,6 +12,34 @@ from PIL import ImageTk, Image
 import tkinter.font as tkFont
 
 
+# Custom entry with hint/placeholder
+class Hintry(tk.Entry):
+    def __init__(self, master=None, hint="Hint", color="grey", *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.hint = hint
+
+        self.fg_color = self["fg"]
+        self.hint_color = color
+
+        self.bind("<FocusIn>", self.clear_hint)
+        self.bind("<FocusOut>", self.renew_hint)
+
+        self.input_hint()
+
+    def input_hint(self):
+        self.insert("0", self.hint)
+        self["fg"] = self.hint_color
+
+    def clear_hint(self, a):
+        if self["fg"] == self.hint_color:
+            self.delete("0", "end")
+            self["fg"] = self.fg_color
+
+    def renew_hint(self, a):
+        if not self.get():
+            self.input_hint()
+
+
 # Initializing window
 class Calculator:
     def __init__(self):
@@ -19,7 +47,6 @@ class Calculator:
         self.root.minsize(850, 778)
         self.root.title("Satisfactory Production Calculator")
         self.root.configure(bg="#5F668C")
-        self.root.bind('<Configure>', self.Resize)
 
         # Placing the background image in the center of the screen
         self.canvas = tk.Canvas(self.root, width=850, height=778, highlightthickness=0)
@@ -28,44 +55,50 @@ class Calculator:
         self.canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         # Entry based overclocking
-        self.overclockIn = tk.Entry(bd=0, fg="#E59345", width=9, font=tk.font.Font(family="Myriad Pro", size=20))
+        self.overclockIn = tk.Entry(master=self.canvas, bd=0, fg="#E59345", width=9, font=tk.font.Font(family="Myriad Pro", size=20))
         self.overclockIn.insert(0, "100.0000%")
         self.overclockIn.place(x=175, y=593)
 
         self.root.bind("<Return>", (lambda event: self.ValidateOverclock(self.overclockIn.get())))
 
-        self.overclockValid = tk.Label(bd=0, fg="#278E3A", text="The overclock percentage is valid!")
+        self.overclockValid = tk.Label(master=self.canvas, bd=0, fg="#278E3A", text="The overclock percentage is valid!")
         self.overclockValid.place(x=435, y=593)
 
         # Slider based overclocking
-        self.overclockSlider = tk.Scale(command=self.ValidateOverclock, troughcolor="#FA9549", orient="horizontal",
-                                        from_=0.0, to=250.0, length=500, width=45, sliderlength=10, bd=5,
-                                        highlightthickness=0, tickinterval=50.0, showvalue=0)
+        self.overclockSlider = tk.Scale(master=self.canvas, command=self.ValidateOverclock, troughcolor="#FA9549",
+                                        orient="horizontal", from_=0.0, to=250.0, length=500, width=45, sliderlength=10,
+                                        bd=5, highlightthickness=0, tickinterval=50.0, showvalue=0)
         self.overclockSlider.set(100)
         self.overclockSlider.place(x=170, y=639)
 
-        # All entries for in and output amounts
-        self.entryFont = tk.font.Font(family="Myriad Pro", size=10)
+        # Frames to hold input/output entries and labels so they properly resize
+        self.inputAFrame = tk.Frame(master=self.canvas, bd=0)
+        self.inputBFrame = tk.Frame(master=self.canvas, bd=0)
+        self.inputCFrame = tk.Frame(master=self.canvas, bd=0)
+        self.inputDFrame = tk.Frame(master=self.canvas, bd=0)
+        self.outputAFrame = tk.Frame(master=self.canvas, bd=0)
+        self.outputBFrame = tk.Frame(master=self.canvas, bd=0)
 
-        self.inputA = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
+        # All entries for in and output amounts
+        self.entryFont = tk.font.Font(family="Myriad Pro", size=8)
+
+        self.inputA = Hintry(master=self.canvas, hint="?", bd=0, fg="#E7994F", width=6, font=self.entryFont)
         #self.inputB = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
         #self.inputC = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
         #self.inputD = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
-        self.outputA = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
+        self.outputA = Hintry(master=self.canvas, hint="?", bd=0, fg="#E7994F", width=9, font=self.entryFont)
         #self.outputB = tk.Entry(bd=0, fg="#E7994F", width=9, font=self.entryFont)
 
+        self.inputA.place(x=97, y=200)
+        #self.inputB.place()
+        #self.inputC.place()
+        #self.inputD.place()
+        self.outputA.place(x=575, y=300)
+        #self.outputB.place()
 
         self.root.mainloop()
 
     # function to validate the input number for overclock percentage
-
-    def Resize(self, wh):
-        w = (1920-self.root.winfo_width())/2
-        h = (1040-self.root.winfo_height())/2
-        self.overclockIn.place(x=710-w, y=724-h)
-        self.overclockValid.place(x=970-w, y=724-h)
-        self.overclockSlider.place(x=705-w, y=770-h)
-
     def ValidateOverclock(self, overclock_var):
         try:
             if 250.0 >= float(overclock_var.strip("%")) >= 0.0:
